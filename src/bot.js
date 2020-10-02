abc0e6962bababb420f1129236e3c7539248f530
tkmarsten <tkmarsten@gmail.com>
2020-10-02 14:40:16 -0700

    Initial commit

---
+++ src/bot.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+const { Client } = require('discord.js');
+const client = new Client();
+
+client.on('ready', () => {
+    console.log(`${client.user.tag} has logged in.`);
+});
+
+client.on('message', (message) => {
+    if (message.author.bot) return;
+    console.log(`[${message.author.tag}]: ${message.content}`);
+    if (message.content === 'hello') {
+        message.channel.send('hello');
+    }
+});
+
+client.login(process.env.KENDO_BOT_TOKEN);
