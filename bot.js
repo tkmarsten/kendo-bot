e16c0f266c774e788e2a31b52b729eb9f1389700
tkmarsten <tkmarsten@gmail.com>
2020-10-05 22:40:57 -0700

    ESLinter

--- bot.js
+++ bot.js
@@ -1,8 +1,15 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const { prefix } = require('./config.json');
 
 bot.on('ready', () => {
     console.log(`${bot.user.tag} is online.`);
 });
 
 bot.login(process.env.token);
+
+bot.on('message', message => {
+    if (message.content === `${prefix}server`) {
+        message.channel.send(`Server name: ${message.guild.name}`);
+    }
+});
