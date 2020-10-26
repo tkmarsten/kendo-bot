a3bd2398d5c16f20f150080c63bdf7c057d6a93d
tkmarsten <tkmarsten@gmail.com>
2020-10-25 19:38:12 -0700

    Added command arguments

--- bot.js
+++ bot.js
@@ -9,6 +9,15 @@ bot.on('ready', () => {
 bot.login(process.env.token);
 
 bot.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (command === 'args') {
+        if (!args.length) {
+            return message.channel.send(`You have not input any arguments, ${message.author}!`);
+        }
+    }
     if (message.content === `${prefix}server`) {
         message.channel.send(`Server name: ${message.guild.name}`);
     }
