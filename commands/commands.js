0cd71807ee7be2668976e0641e3778ea09850428
tkmarsten <tkmarsten@gmail.com>
2020-10-25 23:05:25 -0700

    Fixed no message send

--- commands/commands.js
+++ commands/commands.js
@@ -11,6 +11,8 @@ module.exports = {
             data.push('Here\'s a list of all my commands:');
             data.push(commands.map(command => command.name).join(', '));
             data.push(`\nYou can send \`${prefix}commands [command name]\` to get info on a specific command!`);
+
+            return message.author.reply(data);
         }
     },
 };
