69ea1b4beba256a3a44e0139a96e5f0d615b64b6
tkmarsten <tkmarsten@gmail.com>
2020-10-25 21:46:01 -0700

    Updated zoom command

--- commands/zoom.js
+++ commands/zoom.js
@@ -2,4 +2,18 @@ const Discord = require('discord.js');
 
 module.exports = {
     name: 'zoom',
+    description: 'Zoom practice',
+
+    execute(message, args) {
+        const zoom = new Discord.MessageEmbed()
+            .setColor('#55aac9')
+            .setTitle('Zoom Practice')
+            .addFields(
+                { name: 'Days', value: 'Wednesday, Friday' },
+                { name: 'Time', value: '7-8pm PST' },
+                { name: 'Link', value: 'https://washington.zoom.us/j/93417649586' },
+            );
+
+        message.channel.send(zoom);
+    },
 };
