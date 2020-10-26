7c536b8079bc61fccd5a9ab6c6fcc2e9f214b8cd
tkmarsten <tkmarsten@gmail.com>
2020-10-25 23:48:20 -0700

    Changed punctuation

--- commands/commands.js
+++ commands/commands.js
@@ -8,7 +8,7 @@ module.exports = {
         const { commands } = message.client;
 
         if (!args.length) {
-            data.push('Here\'s a list of all my commands:');
+            data.push('here\'s a list of all my commands:');
             data.push(commands.map(command => command.name).join(', '));
             data.push(`\nYou can send \`${prefix}commands [command name]\` to get info on a specific command!`);
 
