9cac833f85a89a898a43226ae667de8b3f374794
tkmarsten <tkmarsten@gmail.com>
2020-10-25 21:17:49 -0700

    Added help command

---
+++ commands/help.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const { prefix } = require('../config.json');
+
+module.exports = {
+    name: 'help',
+    description: 'Bot information',
+
+    execute(message, args) {
+        const botInfo = new Discord.MessageEmbed()
+            .setColor('#55aac9')
+            .setTitle('Bot Information')
+            .setDescription('Learn about Bot Senpai')
+            .addFields(
+                { name: 'Prefix', value: `${prefix}` },
+                { name: 'Commands', value: 'server' },
+            );
+
+        Discord.Channel.send(botInfo);
+    },
+};
