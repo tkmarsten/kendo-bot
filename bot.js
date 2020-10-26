ca4c9a19283d53ad0a5f7d9abbb5a60d1c09f84d
tkmarsten <tkmarsten@gmail.com>
2020-10-25 21:17:33 -0700

    DM message catch

--- bot.js
+++ bot.js
@@ -1,34 +1,42 @@
+/* eslint-disable brace-style */
 const Discord = require('discord.js');
-const bot = new Discord.Client();
+const fs = require('fs');
 const { prefix } = require('./config.json');
 
-const fs = require('fs');
+const bot = new Discord.Client();
 bot.commands = new Discord.Collection();
+
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-bot.on('ready', () => {
-    console.log(`${bot.user.tag} is online.`);
-});
-
-bot.login(process.env.token);
 
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     bot.commands.set(command.name, command);
 }
 
+bot.on('ready', () => {
+    console.log(`${bot.user.tag} is online.`);
+});
 
 bot.on('message', message => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
-    const args = message.content.slice(prefix.length).trim().split(/ +/);
-    const command = args.shift().toLowerCase();
 
-    if (!bot.commands.has(command)) return;
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const commandName = args.shift().toLowerCase();
+
+    if (!bot.commands.has(commandName)) return;
+
+    const command = bot.commands.get(commandName);
+
     try {
-        bot.commands.get(command).execute(message, args);
-    }
-    catch (error) {
+        command.execute(message, args);
+    } catch (error) {
         console.error(error);
         message.reply('There was an error executing that command!');
     }
+
+    if (message.channel.type === 'dm') {
+        return message.reply('I cannot execute commands inside DMs.');
+    }
 });
+
+bot.login(process.env.token);
