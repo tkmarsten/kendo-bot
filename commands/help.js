4425373ea02d4146923db6dff5fabf662d781af9
tkmarsten <tkmarsten@gmail.com>
2020-10-25 22:47:42 -0700

    Removed commands in embed

--- commands/help.js
+++ commands/help.js
@@ -3,16 +3,15 @@ const { prefix } = require('../config.json');
 
 module.exports = {
     name: 'help',
-    description: 'Bot information',
 
     execute(message, args) {
         const botInfo = new Discord.MessageEmbed()
             .setColor('#55aac9')
             .setTitle('Bot Information')
-            .setDescription('Learn about Bot Senpai')
+            .setDescription('Bot Senpai, Hanshi 8 Dan')
             .addFields(
+                { name: 'Creator', value: 'Tiarnan' },
                 { name: 'Prefix', value: `${prefix}` },
-                { name: 'Commands', value: 'server' },
             );
 
         message.channel.send(botInfo);
